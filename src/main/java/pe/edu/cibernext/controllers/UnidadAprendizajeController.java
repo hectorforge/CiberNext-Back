5fec6e203aac5718b7f406476ccd116aa09005aa
Jesus Lopez Juscamaita <i202410080@cibertec.edu.pe>
2025-08-10 20:45:15 -0500

    Update UnidadAprendizajeController.java

--- src/main/java/pe/edu/cibernext/controllers/UnidadAprendizajeController.java
+++ src/main/java/pe/edu/cibernext/controllers/UnidadAprendizajeController.java
@@ -18,40 +18,54 @@ public class UnidadAprendizajeController {
 
     private final UnidadAprendizajeService unidadService;
 
-    @GetMapping("/obtenerPorId/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UnidadAprendizajeRespuestaDto> obtenerPorId(@PathVariable Long id) {
         return ResponseEntity.ok(unidadService.buscarPorId(id));
     }
 
-    @GetMapping("/listarTodos")
+    @GetMapping
     public ResponseEntity<List<UnidadAprendizajeRespuestaDto>> listarTodos() {
         return ResponseEntity.ok(unidadService.listarTodos());
     }
 
-    @GetMapping("/listarPorCurso/{cursoId}")
+    // TODO:  Debe mostrar la estructura jerárquica de las unidades de aprendizaje contenidas dentro de esta unidad
+    @GetMapping("/curso/{cursoId}")
     public ResponseEntity<List<UnidadAprendizajeRespuestaDto>> listarPorCurso(@PathVariable Long cursoId) {
         return ResponseEntity.ok(unidadService.listarPorCurso(cursoId));
     }
 
-    @PostMapping("/registrar")
+    @PostMapping
     public ResponseEntity<UnidadAprendizajeDto> registrar(@RequestBody UnidadAprendizajeDto dto) {
         return ResponseEntity.ok(unidadService.registrar(dto));
     }
 
-    @PutMapping("/actualizar/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<UnidadAprendizajeDto> actualizar(@RequestBody UnidadAprendizajeDto dto) {
         return ResponseEntity.ok(unidadService.actualizar(dto));
     }
 
-    @DeleteMapping("/eliminar/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         unidadService.eliminarPorId(id);
         return ResponseEntity.noContent().build();
     }
 
+    // TODO: Para que se utiliza este endopoint?
     @GetMapping("/jerarquico")
     public List<UnidadAprendizajeJerarquicoDto> listarJerarquico() {
         return unidadService.listarJerarquico();
     }
 
 }
+
+
+// TODO: Falta implementar
+/*
+ - [Copiar a CursoController]
+   Listar unidades de aprendizaje por curso.
+ - Listar consultas por unidad de aprendizaje
+   (mostrar la unidad de aprendizaje a la que pertenece la consulta. también mostrar las consultas jerarquicamente: curso -> unidad -> consulta)
+ - Listar documentos por unidad de aprendizaje
+   (mostrar la unidad de aprendizaje a la que pertenece el documento. también mostrar los documentos jerarquicamente: curso -> unidad -> documento)
+
+*/
